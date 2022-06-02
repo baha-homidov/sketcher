fdceae6ce23c880dece8d6257479370b3d28c8d6
Mirzobakhtiyor Homidov <khamidov.mirzo@gmail.com>
2022-06-02 11:21:47 +0800

    add sketch on mouseover

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 const canvas = document.querySelector(".canvas");
 
 
+function createCanvas(gridNum) {
 
-
-
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement("div");
-    row.classList.add(`row-${i + 1}`);
-    row.style.cssText = "flex-grow: 1; display: flex; border: 1px solid red";
-    for (let j = 0; j < 16; j++) {
-        const column = document.createElement("div");
-        column.classList.add(`column-${j + 1}`);
-        column.style.cssText = "flex-grow: 1; border: 1px solid red";
-        row.appendChild(column);
+    for (let i = 0; i < gridNum; i++) {
+        const row = document.createElement("div");
+        row.classList.add(`row-${i + 1}`);
+        row.style.cssText = "flex-grow: 1; display: flex;";
+        
+        for (let j = 0; j < gridNum; j++) {
+            const column = document.createElement("div");
+            column.classList.add(`column-${j + 1}`);
+            column.style.cssText = "flex-grow: 1;";
+            column.onmouseover = () => {
+                column.style.backgroundColor = "black";
+            };
+            row.appendChild(column);
+        }
+        canvas.appendChild(row);
     }
-    canvas.appendChild(row);
+
 }
+
+createCanvas(64);
 
